9a775dc4d04e30a77c1369b1a073d95de63d50a5
Vitor Natal <vitornc31@gmail.com>
2025-09-28 12:35:15 -0300

    First Commit - DB connection

--- src/src/Main.java
+++ src/src/Main.java
@@ -5,7 +5,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class Main {
@@ -21,17 +23,23 @@ public class Main {
         var dataSource = new MysqlDataSource();
         dataSource.setServerName(prop.getProperty("serverName"));
         dataSource.setPort(Integer.parseInt(prop.getProperty("port")));
-
+        dataSource.setPassword(prop.getProperty("password"));
+        dataSource.setDatabaseName(prop.getProperty("databaseName"));
         try(Connection connection = dataSource.getConnection(
                 prop.getProperty("user"),
                 System.getenv("MYSQL_PASS")
         )){
-            System.out.println("Sucesso");
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT artist_name FROM artists WHERE artist_name LIKE 'I%'");
+
+            while(rs.next()){
+                String name = rs.getString("artist_name");
+                System.out.println(name);
+            }
         }catch(SQLException e){
             throw new RuntimeException(e);
         }
 
-        dataSource.setPassword(prop.getProperty("password"));
-        dataSource.setDatabaseName(prop.getProperty("databaseName"));
+
     }
 }
